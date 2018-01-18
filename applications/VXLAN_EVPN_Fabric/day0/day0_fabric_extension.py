ba59665ff88117850bfa6e85f37ed6d94aeb3a43
Luis Flores <luflores@cisco.com>
2018-01-18 16:51:33 -0500

    Update fab Data

--- applications/VXLAN_EVPN_Fabric/day0/day0_fabric_extension.py
+++ applications/VXLAN_EVPN_Fabric/day0/day0_fabric_extension.py
@@ -1,5 +1,5 @@
 from applications.VXLAN_EVPN_Fabric.credentials import URL, CERT, LOGIN, PASSWORD
-from applications.VXLAN_EVPN_Fabric.fabric_data import FABRIC_EXTENSION, FABRIC_OVERLAY_EXT
+from applications.VXLAN_EVPN_Fabric.fabric_data import UNDERLAY_FABRIC_EXTENSION, OVERLAY_FABRIC_EXTENSION
 from dcnmtoolkit import Session
 import logging
 import json
@@ -18,13 +18,13 @@ def main(url=None, cert=None):
 
 
 def add_fab_ext(session):
-    for underlay in FABRIC_EXTENSION:
+    for underlay in UNDERLAY_FABRIC_EXTENSION:
         url = '/rest/top-down/fabrics/%s/vrf-extension' % underlay['sourceFabric']
         resp = session.post(url, json.dumps(underlay))
         time.sleep(10)
         logging.info('HTTP POST response %s' % resp)
 
-    for overlay in FABRIC_OVERLAY_EXT:
+    for overlay in OVERLAY_FABRIC_EXTENSION:
         url = '/rest/top-down/fabrics/%s/vrf-extension' % overlay['sourceFabric']
         resp = session.post(url, json.dumps(overlay))
         time.sleep(10)
@@ -35,7 +35,7 @@ def del_fab_ext(session):
     fabric_ext = dict()
     fabric_ext_keys = dict.fromkeys(['sourceFabric', 'sourceSwitch', 'sourcePort', 'destFabric', 'destSwitch',
                                      'destPort', 'extensionType'])
-    for underlay in FABRIC_EXTENSION:
+    for underlay in UNDERLAY_FABRIC_EXTENSION:
         for key, value in underlay.iteritems():
             if key in fabric_ext_keys:
                 fabric_ext[key] = value
@@ -44,7 +44,7 @@ def del_fab_ext(session):
         logging.info('HTTP POST response %s' % resp)
         print resp.content
 
-    for overlay in FABRIC_OVERLAY_EXT:
+    for overlay in OVERLAY_FABRIC_EXTENSION:
         for key, value in overlay.iteritems():
             if key in fabric_ext_keys:
                 fabric_ext[key] = value
--- applications/VXLAN_EVPN_Fabric/day1/day1_network_deployment.py
+++ applications/VXLAN_EVPN_Fabric/day1/day1_network_deployment.py
@@ -15,6 +15,8 @@ def main(url=None, cert=None):
     special_keys = ["vrfTemplateConfig", "networkTemplateConfig"]
     ipv4_base = u'1.0.0.1'
     ipv6_base = u'c5c0::1'
+    fabrics = ['3', '4']
+
     overlay = {
         "fabric": "Fab2",
         "vrfExtensionTemplate": "Default_VRF_Extension",
@@ -29,41 +31,41 @@ def main(url=None, cert=None):
             "vrfVlanId": 2
         }
     }
-    fabric = '4'
-    mcast_grp = '239.255.25' + fabric + '.0'
-    for network_id in range(101, 106):
-        network_params = {
-            "fabric": 'Fab' + str(fabric),
-            "networkExtensionTemplate": "Default_Network_Extension",
-            "networkId": "20" + str(network_id),
-            "networkName": "Network" + str(network_id),
-            "networkTemplate": "Default_Network",
-            "networkTemplateConfig": {
-                "dhcpServerAddr1": "",
-                "enableIR": False,
-                "gatewayIpAddress": str(ipaddress.IPv4Address(ipv4_base) + 65536 * network_id) + '/22',
-                "gatewayIpV6Address": str(ipaddress.IPv6Address(ipv6_base) + int(math.pow(256, 12)) * network_id) + '/64',
-                "intfDescription": "",
-                "isLayer2Only": False,
-                "mcastGroup": mcast_grp,
-                "mtu": "",
+    for fabric in fabrics:
+        mcast_grp = '239.255.25' + fabric + '.0'
+        for network_id in range(101, 106):
+            network_params = {
+                "fabric": 'Fab' + str(fabric),
+                "networkExtensionTemplate": "Default_Network_Extension",
+                "networkId": "20" + str(network_id),
                 "networkName": "Network" + str(network_id),
-                "nveId": 1,
-                "segmentId": "20" + str(network_id),
-                "suppressArp": False,
-                "vlanId": network_id,
-                "vrfDhcp": "",
-                "vrfName": "OVERLAY1"
-            },
-            "vrf": "OVERLAY1"
-        }
-        network_params['networkTemplateConfig'] = json.dumps(network_params['networkTemplateConfig'])
-        url = '/rest/top-down/fabrics/%s/networks' % ('Fab' + str(fabric))
-        resp = session.post(url, json.dumps(network_params))
-        if not resp.ok:
-            print json.dumps(json.loads(resp.content), indent=4)
-        logging.info('HTTP POST response %s' % resp)
-        # print json.dumps(network_params, indent=4, sort_keys=True)
+                "networkTemplate": "Default_Network",
+                "networkTemplateConfig": {
+                    "dhcpServerAddr1": "",
+                    "enableIR": False,
+                    "gatewayIpAddress": str(ipaddress.IPv4Address(ipv4_base) + 65536 * network_id) + '/22',
+                    "gatewayIpV6Address": str(ipaddress.IPv6Address(ipv6_base) + int(math.pow(256, 12)) * network_id) + '/64',
+                    "intfDescription": "",
+                    "isLayer2Only": False,
+                    "mcastGroup": mcast_grp,
+                    "mtu": "",
+                    "networkName": "Network" + str(network_id),
+                    "nveId": 1,
+                    "segmentId": "20" + str(network_id),
+                    "suppressArp": False,
+                    "vlanId": network_id,
+                    "vrfDhcp": "",
+                    "vrfName": "OVERLAY1"
+                },
+                "vrf": "OVERLAY1"
+            }
+            network_params['networkTemplateConfig'] = json.dumps(network_params['networkTemplateConfig'])
+            url = '/rest/top-down/fabrics/%s/networks' % ('Fab' + str(fabric))
+            resp = session.post(url, json.dumps(network_params))
+            if not resp.ok:
+                print json.dumps(json.loads(resp.content), indent=4)
+            logging.info('HTTP POST response %s' % resp)
+            # print json.dumps(network_params, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
